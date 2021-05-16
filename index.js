b0cfc015bd3b81b001d2547802445e8674767bd6
Mohit Gupta <09mohit1994@gmail.com>
2021-05-16 20:45:11 +0530

    Initiated taking inputs in an interactive manner

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
 require("dotenv").config();
 
-const {checkSlotAvailability} = require("./services/slotAvailability");
+const { checkSlotAvailability } = require("./services/slotAvailability");
 
-checkSlotAvailability("Gwalior", 1);
+// const readline = require("readline");
+// const { execSync } = require("child_process");
+
+// const { logInfo } = require("./services/logger");
+// const citiesList = require("./data/cities.json");
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// const ask = (questionType) => {
+//   switch (questionType) {
+//     case "City":
+//       rl.question(
+//         "\nWhich district you are in, enter the name? (For example: Gwalior)\n",
+//         (city) => {
+//           if (!citiesList[city]) {
+//             logInfo(
+//               "City not found. Please enter the exact name as on Cowin website"
+//             );
+//             ask("City");
+//           } else {
+//             execSync(`export CITY=${city}`);
+//             ask("AgeGroup");
+//           }
+//         }
+//       );
+
+//     case "AgeGroup":
+//       rl.question(
+//         "\nYou want to monitor for 45+ or for 18+, enter either 18 or 45?\n",
+//         (ageGroup) => {
+//           if (ageGroup !== "45" && ageGroup !== "18") {
+//             logInfo("Please enter the correct age group. Either 18 or 45");
+//             ask("AgeGroup");
+//           } else {
+//             execSync(`export AGE_GROUP=${ageGroup}`);
+//             ask("DateOffset");
+//           }
+//         }
+//       );
+
+//     case "DateOffset":
+//       rl.question(
+//         "\nEnter 0 if you want to monitor for today, enter 1 for tomorrow, 2 for day after tomorrow. Values accepted from 0-6\n",
+//         (dateOffset) => {
+//           if (dateOffset < 0 || dateOffset > 6) {
+//             logInfo("Please enter a value between 0 and 6 only");
+//             ask("DateOffset");
+//           } else {
+//             execSync(`export DATE_OFFSET=${parseInt(dateOffset)}`);
+//             checkSlotAvailability(process.env.CITY, process.env.DATE_OFFSET);
+//           }
+//         }
+//       );
+//   }
+// };
+
+// ask("City");
+
+checkSlotAvailability("Gwalior", 0);
--- services/url.js
+++ services/url.js
@@ -5,7 +5,8 @@ function getUrlByPincode(pincode = 474002) {
   const d = new Date();
   const dateFormatted = `17-${d.getMonth() + 1}-${d.getFullYear()}`;
 
-  const baseUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin";
+  const baseUrl =
+    "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin";
   const fullUrl = `${baseUrl}?pincode=${pincode}&date=${dateFormatted}`;
 
   logInfo(`Checking for vaccine slots in "${pincode}" for "${dateFormatted}"`);
@@ -15,18 +16,24 @@ function getUrlByPincode(pincode = 474002) {
 
 function getUrlByDistrictName(districtName = "Gwalior", daysOffset = 0) {
   const d = new Date();
-  const dateFormatted = `${d.getDate() + daysOffset}-${d.getMonth() + 1}-${d.getFullYear()}`;
+  const day = d.getDate() + parseInt(daysOffset);
+  const month = d.getMonth() + 1;
+  const year = d.getFullYear();
+  const dateFormatted = `${day}-${month}-${year}`;
   const districtId = citiesList[districtName];
 
-  const baseUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict";
+  const baseUrl =
+    "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict";
   const fullUrl = `${baseUrl}?district_id=${districtId}&date=${dateFormatted}`;
 
-  logInfo(`Checking for vaccine slots in "${districtName}" for "${dateFormatted}"`);
+  logInfo(
+    `Checking for vaccine slots in "${districtName}" for "${dateFormatted}"`
+  );
 
   return fullUrl;
 }
 
 module.exports = {
   getUrlByDistrictName,
-  getUrlByPincode
-}
+  getUrlByPincode,
+};
