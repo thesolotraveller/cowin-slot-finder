16aa729262abb350074b6fa3c83b3f6b8f2054a2
Mohit Gupta <09mohit1994@gmail.com>
2021-05-16 16:24:14 +0530

    Added OS notifier as a fallback in case email cannot be configured successfully

---
+++ services/notifier.js
@@ -0,0 +1,23 @@
+const notifier = require('node-notifier');
+const { logError } = require("./logger");
+const { sendEmail } = require("./email");
+
+async function notify(vaccinationCenters) {
+  const { SMTP_SERVER, USER_EMAIL, USER_PASSWORD } = process.env;
+
+  if (!SMTP_SERVER || !USER_EMAIL || !USER_PASSWORD) {
+    logError("Email notifier not configured");
+  } else {
+    // send an email to notify user
+    await sendEmail(vaccinationCenters);
+  }
+
+  notifier.notify({
+    message: 'Vaccination slots available now in your city | Hurry up and book a slot',
+    title: 'Vaccines Available'
+  });
+}
+
+module.exports = {
+  notify
+}
--- services/slotAvailability.js
+++ services/slotAvailability.js
@@ -2,7 +2,7 @@ const request = require("request-promise");
 
 const { logInfo, logSuccess, logError } = require("./logger");
 const { getUrlByDistrictName } = require("./url");
-const { sendEmail } = require("./email");
+const { notify } = require("./notifier");
 
 async function checkSlotAvailability(city, dateOffsetFromToday = 0) {
   try {
@@ -21,15 +21,16 @@ async function checkSlotAvailability(city, dateOffsetFromToday = 0) {
         };
       });
 
-    if (availableCenters.length > 0) {
-      logSuccess("\nSlots are available", availableCenters);
-      await sendEmail(availableCenters);
-      logSuccess("Slots available. You have just been notified via email. Trying again in 60 seconds...\n");
-      setTimeout(() => checkSlotAvailability(city, dateOffsetFromToday), 60000);
-    } else {
-      logInfo("Slots are not available. Trying again in 3 seconds...\n");
-      setTimeout(() => checkSlotAvailability(city, dateOffsetFromToday), 3000);
-    }
+    await notify(availableCenters);
+    // if (availableCenters.length > 0) {
+    //   logSuccess("\nSlots are available", availableCenters);
+    //   await notify(availableCenters);
+    //   logSuccess("Slots available. You have just been notified via email. Trying again in 60 seconds...\n");
+    //   setTimeout(() => checkSlotAvailability(city, dateOffsetFromToday), 60000);
+    // } else {
+    //   logInfo("Slots are not available. Trying again in 3 seconds...\n");
+    //   setTimeout(() => checkSlotAvailability(city, dateOffsetFromToday), 3000);
+    // }
   } catch (e) {
     logError(e, "\nSomething is wrong. Tracker stopped\n");
     setTimeout(() => checkSlotAvailability(city, dateOffsetFromToday), 3000);
